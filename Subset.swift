75766a5658fdceedeffcd1dea9bb5a4e9a2dfa91
Saumen <saumenroy323@gmail.com>
2019-04-04 16:53:39 +0600

    Add files via upload

---
+++ Subset.swift
@@ -0,0 +1,15 @@
+class Solution {
+   func subsets(_ nums: [Int]) -> [[Int]] {
+        var ans:[[Int]] = []
+        for i in 0..<nums.count{
+            for j in 0..<ans.count{
+                var temp = ans[j]
+                temp.append(nums[i])
+                ans.append(temp)
+            }
+            ans.append([nums[i]])
+        }
+        ans.append([])
+        return ans
+    }
+}
